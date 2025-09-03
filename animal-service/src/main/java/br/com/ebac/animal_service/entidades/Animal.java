a661ba8275e1205b68487dbb59f678d9fa6d3699
wellington <wellington.h.s.oliveira@hotmail.com>
2025-09-02 23:43:59 -0300

    criei a entidade para dar forma a aplicaçao e fiz o mapeamento jpa

---
+++ animal-service/src/main/java/br/com/ebac/animal_service/entidades/Animal.java
@@ -0,0 +1,118 @@
+package br.com.ebac.animal_service.entidades;
+
+import jakarta.persistence.*;
+
+import java.sql.Date;
+
+@Entity
+@Table(name = "Animal")
+public class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nomeProvisorio;
+
+    @Column(nullable = false)
+    private Integer idadeEstimada;
+
+    @Column(nullable = false)
+    private String raca;
+
+    @Column(nullable = false)
+    private Date dataEntrada;
+
+    @Column
+    private Date dataAdocao;
+
+    @Column(nullable = false)
+    private String condicoesChegada;
+
+    @Column(nullable = false)
+    private String nomeRecebedor;
+
+    @Column
+    private Date dataObito ;
+
+    @Column(nullable = false)
+    private String porte;
+
+    public String getPorte() {
+        return porte;
+    }
+
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+
+    public Date getDataObito() {
+        return dataObito;
+    }
+
+    public void setDataObito(Date dataObito) {
+        this.dataObito = dataObito;
+    }
+
+    public String getNomeRecebedor() {
+        return nomeRecebedor;
+    }
+
+    public void setNomeRecebedor(String nomeRecebedor) {
+        this.nomeRecebedor = nomeRecebedor;
+    }
+
+    public String getCondicoesChegada() {
+        return condicoesChegada;
+    }
+
+    public void setCondicoesChegada(String condicoesChegada) {
+        this.condicoesChegada = condicoesChegada;
+    }
+
+    public Date getDataAdocao() {
+        return dataAdocao;
+    }
+
+    public void setDataAdocao(Date dataAdocao) {
+        this.dataAdocao = dataAdocao;
+    }
+
+    public Date getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(Date dataEntrada) {
+        this.dataEntrada = dataEntrada;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public Integer getIdadeEstimada() {
+        return idadeEstimada;
+    }
+
+    public void setIdadeEstimada(Integer idadeEstimada) {
+        this.idadeEstimada = idadeEstimada;
+    }
+
+    public String getNomeProvisorio() {
+        return nomeProvisorio;
+    }
+
+    public void setNomeProvisorio(String nomeProvisorio) {
+        this.nomeProvisorio = nomeProvisorio;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
