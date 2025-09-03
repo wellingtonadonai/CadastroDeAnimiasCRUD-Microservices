5651de134482b37a3340723c40c0ebf07c726b1b
wellington <wellington.h.s.oliveira@hotmail.com>
2025-09-02 23:43:01 -0300

    criei dados oara minha tabela

---
+++ animal-service/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO Animal (
+    nome_provisorio,
+    idade_estimada,
+    raca,
+    data_entrada,
+    condicoes_chegada,
+    nome_recebedor,
+    porte
+) VALUES
+('doguinho', 5, 'pastor alemao', CURRENT_DATE, 'bem pulguento', 'wellington', 'grande'),
+('gatinho', 2, 'siamês', CURRENT_DATE, 'resgatado da rua', 'joão', 'pequeno'),
+('bolota', 7, 'vira-lata', CURRENT_DATE, 'machucado', 'maria', 'médio');
