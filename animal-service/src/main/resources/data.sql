1ce726c15376a259f2b9e4b41e789e9400a72bf2
wellington <wellington.h.s.oliveira@hotmail.com>
2025-09-03 23:01:08 -0300

    alterei os dados para entradas de outros animais

--- animal-service/src/main/resources/data.sql
+++ animal-service/src/main/resources/data.sql
@@ -7,6 +7,5 @@ INSERT INTO Animal (
     nome_recebedor,
     porte
 ) VALUES
-('doguinho', 5, 'pastor alemao', CURRENT_DATE, 'bem pulguento', 'wellington', 'grande'),
-('gatinho', 2, 'siamês', CURRENT_DATE, 'resgatado da rua', 'joão', 'pequeno'),
-('bolota', 7, 'vira-lata', CURRENT_DATE, 'machucado', 'maria', 'médio');
+('Cachorro', 5, 'pastor alemao', CURRENT_DATE, 'bem pulguento', 'wellington', 'grande'),
+('Gato', 2, 'siamês', CURRENT_DATE , 'resgatado da rua', 'joão', 'pequeno'),
